ba3f907ddf5f7fcca3f81d69d0fde99687c571a1
Marcelo Cohen <marcelo.cohen@pucrs.br>
2023-10-25 15:11:49 -0300

    Correção no método para obter adjacentes em Python, para retornar lista vazia caso o vértice não exista

--- digraph.py
+++ digraph.py
@@ -1,40 +1,37 @@
 from graph import Graph
 
+
 class Digraph(Graph):
 
-	def addEdge(self, v, w):
-		super().addToList(v,w)
+    def addEdge(self, v, w):
+        super().addToList(v, w)
 
-	def toDot(self):
-		edges = set()
-		NEWLINE = '\n'
-		sb = "graph {" + NEWLINE
-		sb += "rankdir = LR;" + NEWLINE
-		sb += "node [shape = circle];" + NEWLINE
-		for v in sorted(self.getVerts()):
-			for w in self.getAdj(v):
-				edge = w + v if v > w else v + w
-				if edge not in edges:
-					sb += v + " -> " + w + NEWLINE
-					edges.add(edge)
-		sb += "}" + NEWLINE
-		return sb
+    def toDot(self):
+        NEWLINE = '\n'
+        sb = "graph {" + NEWLINE
+        sb += "rankdir = LR;" + NEWLINE
+        sb += "node [shape = circle];" + NEWLINE
+        for v in sorted(self.getVerts()):
+            for w in self.getAdj(v):
+                sb += v + " -> " + w + NEWLINE
+        sb += "}" + NEWLINE
+        return sb
+
 
 if __name__ == "__main__":
 
-	g = Digraph()
+    g = Digraph()
 
-	g.addEdge("0", "1")
-	g.addEdge("0", "2")
-	g.addEdge("2", "1")
+    g.addEdge("0", "1")
+    g.addEdge("0", "2")
+    g.addEdge("2", "1")
 
-	g = Digraph("tinyG.txt")
+    g = Digraph("tinyG.txt")
 
-	for v in g.getVerts():
-		print(f"{v}: ", end="")
-		for w in g.getAdj(v):
-			print(f"{w} ", end="")
-		print()
-	print()
-	print(g.toDot())	
-
+    for v in g.getVerts():
+        print(f"{v}: ", end="")
+        for w in g.getAdj(v):
+            print(f"{w} ", end="")
+        print()
+    print()
+    print(g.toDot())
