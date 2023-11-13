8cea3e594a9b0cce1632ebd8e8ad6a881a99caed
Marcelo Cohen <mflashbr@gmail.com>
2023-11-13 20:29:25 -0300

    Correção do exercício de comparação de caminhos mínimos

--- AppShortestPathComparison.java
+++ AppShortestPathComparison.java
@@ -4,17 +4,19 @@ public class AppShortestPathComparison {
     public static void main(String[] args) {
 
         EdgeWeightedDigraph DG = null;
-        boolean leitura = true;
+        // true para ler do arquivo, false para gerar um grafo
+        boolean leitura = false;
         if (!leitura) {
 
-            // Grafo completo
-            // Digraph dg = DigraphGenerator.complete(1000);
+            // Grafo completo:
+            // Caso em que F-W é MAIS eficiente que Dijkstra
+            Digraph dg = DigraphGenerator.complete(1000);
 
             // Árvore binária
             // Digraph dg = DigraphGenerator.binaryTree(3000);
 
             // DAG
-            Digraph dg = DigraphGenerator.dag(5000, 2000);
+            // Digraph dg = DigraphGenerator.dag(5000, 2000);
 
             DG = new EdgeWeightedDigraph();
             for (String v : dg.getVerts()) {
@@ -29,8 +31,23 @@ public class AppShortestPathComparison {
 
         AdjMatrixEdgeWeightedDigraph G = new AdjMatrixEdgeWeightedDigraph(DG);
 
+        System.out.println("Vértices: " + G.getTotalVertices());
+        System.out.println("Arestas: " + G.getTotalEdges());
+
         // Executa Floyd-Warshall e Dijkstra, compara os tempos
+        FloydWarshall fw = new FloydWarshall(G);
+        System.out.println("Floyd-Warshall: " + fw.tempoTotal());
+
+        long tempoDijsktra = 0;
         // Lembrando que é preciso executar Dijkstra para TODOS os vértices
+        for (String v : DG.getVerts()) {
+            long start = System.currentTimeMillis();
+            Dijkstra d = new Dijkstra(DG, v);
+            long end = System.currentTimeMillis();
+            tempoDijsktra += (end - start);
+        }
+        // Mesmo assim, em 99% dos casos, Dijkstra é melhor!
+        System.out.println("Dijkstra: " + tempoDijsktra);
     }
 
 }
